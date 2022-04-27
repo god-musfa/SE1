d9d64b48874ebdc5a6f75ee20d980d486c87843d
Mustafa Tarik <mustafa.tarik@smail.inf.h-brs.de>
2022-04-27 15:47:31 +0200

    Created Test Class

---
+++ src/test/java/de/hbrs/team89/se1_starter_repo/ParkhausServletTest.java
@@ -0,0 +1,17 @@
+package de.hbrs.team89.se1_starter_repo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkhausServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void locator() {
+    }
+}
