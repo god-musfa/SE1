b6a73377c5e6ad29da140ed24d2df0d84b2cf8eb
abause2s <alexander.bause@smail.inf.h-brs.de>
2022-04-27 16:53:32 +0200

    Add Klasse Parkhaus Add Klasse ParkhausTest

---
+++ src/test/java/de/hbrs/team89/se1_starter_repo/ParkhausTest.java
@@ -0,0 +1,17 @@
+package de.hbrs.team89.se1_starter_repo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkhausTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
