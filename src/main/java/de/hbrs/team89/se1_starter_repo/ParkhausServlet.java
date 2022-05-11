852b76fec621e987f70f5d31b6e0ce564acd43a7
Mustafa Tarik <mustafa.tarik@smail.inf.h-brs.de>
2022-05-11 19:19:54 +0200

    imported json

--- src/main/java/de/hbrs/team89/se1_starter_repo/ParkhausServlet.java
+++ src/main/java/de/hbrs/team89/se1_starter_repo/ParkhausServlet.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
+import com.google.gson.*;
 /**
  * common superclass for all servlets
  * groups all auxiliary common methods used in all servlets
