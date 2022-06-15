a81f0b6557767bd6460c33cd8fb014b5b36a800b
abause2s <alexander.bause@smail.inf.h-brs.de>
2022-06-15 18:59:58 +0200

    params für Fahrzeugtyp aktualisiert

--- src/main/java/base/Car.java
+++ src/main/java/base/Car.java
@@ -12,7 +12,8 @@ public class Car implements CarIF {
     Ticket t;
     public Car( String[] params ){
         this.params = params;
-        ft = FahrzeugtypHelper.getFahrzeug(this.params[5]);
+        System.out.println(this.params[8]);
+        ft = FahrzeugtypHelper.getFahrzeug(this.params[8]);
         t = new Ticket(this.params[4], this.params[1]);
     }
 
