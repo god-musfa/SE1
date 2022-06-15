8ccd68f928bb7a062d413bd1a5a04ca23bafb6e9
abause2s <alexander.bause@smail.inf.h-brs.de>
2022-06-15 18:39:15 +0200

    Add FahrzeugtypHelperTest

--- src/main/java/Multiton/Zweirad.java
+++ src/main/java/Multiton/Zweirad.java
@@ -1,10 +1,9 @@
 package Multiton;
 
 public class Zweirad extends Fahrzeugtyp {
-    private static Fahrzeugtyp instance;
+    private static Fahrzeugtyp instance = new Zweirad();
     private Zweirad() {
-        super(1);
-        instance =  new Zweirad();
+        super(1, 0.5);
     }
 
     public static Fahrzeugtyp getFahrzeugtyp() {
