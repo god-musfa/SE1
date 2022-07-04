56374582d27d223cf2f04f07e7b1d508a28cc825
Mazlum <mazlum.yildirim@smail.inf.h-brs.de>
2022-07-04 17:16:39 +0200

    Codesmell

--- src/test/java/multiton/FahrzeugtypHelperTest.java
+++ src/test/java/multiton/FahrzeugtypHelperTest.java
@@ -6,17 +6,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FahrzeugtypHelperTest {
-    static final String zweirad = "zr";
+    static final String zweirad = "Zweirad";
+    static final String trike = "Trike";
+    static final String quad = "Quad";
+    static final String pkw = "PKW";
+    static final String suv = "SUV";
+    static final String pickup = "Pickup";
 
     @Test
     @DisplayName("Testet ob das Fahrzeug vom erwarteten Typ ist")
     void getFahrzeug() {
-        assertTrue(FahrzeugtypHelper.getFahrzeug("zr") instanceof Zweirad);
-        assertTrue(FahrzeugtypHelper.getFahrzeug("Trike") instanceof Trike);
-        assertTrue(FahrzeugtypHelper.getFahrzeug("Quad") instanceof Quad);
-        assertTrue(FahrzeugtypHelper.getFahrzeug("PKW") instanceof PKW);
-        assertTrue(FahrzeugtypHelper.getFahrzeug("SUV") instanceof SUV);
-        assertTrue(FahrzeugtypHelper.getFahrzeug("Pickup") instanceof Pickup);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(zweirad) instanceof Zweirad);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(trike) instanceof Trike);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(quad) instanceof Quad);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(pkw) instanceof PKW);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(suv) instanceof SUV);
+        assertTrue(FahrzeugtypHelper.getFahrzeug(pickup) instanceof Pickup);
     }
 
     @Test
@@ -28,22 +33,22 @@ class FahrzeugtypHelperTest {
     @Test
     @DisplayName("Prüft die getGebührenfaktor Methode")
     void getGebührenfaktorTest(){
-        assertEquals(0.5,FahrzeugtypHelper.getFahrzeug("zr").getGebuehrenfaktor());
-        assertEquals(0.7,FahrzeugtypHelper.getFahrzeug("Trike").getGebuehrenfaktor());
-        assertEquals(0.8,FahrzeugtypHelper.getFahrzeug("Quad").getGebuehrenfaktor());
-        assertEquals(1.0,FahrzeugtypHelper.getFahrzeug("PKW").getGebuehrenfaktor());
-        assertEquals(1.3,FahrzeugtypHelper.getFahrzeug("SUV").getGebuehrenfaktor());
-        assertEquals(1.5,FahrzeugtypHelper.getFahrzeug("Pickup").getGebuehrenfaktor());
+        assertEquals(0.5,FahrzeugtypHelper.getFahrzeug(zweirad).getGebuehrenfaktor());
+        assertEquals(0.7,FahrzeugtypHelper.getFahrzeug(trike).getGebuehrenfaktor());
+        assertEquals(0.8,FahrzeugtypHelper.getFahrzeug(quad).getGebuehrenfaktor());
+        assertEquals(1.0,FahrzeugtypHelper.getFahrzeug(pkw).getGebuehrenfaktor());
+        assertEquals(1.3,FahrzeugtypHelper.getFahrzeug(suv).getGebuehrenfaktor());
+        assertEquals(1.5,FahrzeugtypHelper.getFahrzeug(pickup).getGebuehrenfaktor());
     }
 
     @Test
     @DisplayName("Prüft die getFlaeche Methode")
     void getFlaecheTest(){
-        assertEquals(1,FahrzeugtypHelper.getFahrzeug("zr").getFlaeche());
-        assertEquals(2,FahrzeugtypHelper.getFahrzeug("Trike").getFlaeche());
-        assertEquals(3,FahrzeugtypHelper.getFahrzeug("Quad").getFlaeche());
-        assertEquals(4,FahrzeugtypHelper.getFahrzeug("PKW").getFlaeche());
-        assertEquals(5,FahrzeugtypHelper.getFahrzeug("SUV").getFlaeche());
-        assertEquals(6,FahrzeugtypHelper.getFahrzeug("Pickup").getFlaeche());
+        assertEquals(1,FahrzeugtypHelper.getFahrzeug(zweirad).getFlaeche());
+        assertEquals(2,FahrzeugtypHelper.getFahrzeug(trike).getFlaeche());
+        assertEquals(3,FahrzeugtypHelper.getFahrzeug(quad).getFlaeche());
+        assertEquals(4,FahrzeugtypHelper.getFahrzeug(pkw).getFlaeche());
+        assertEquals(5,FahrzeugtypHelper.getFahrzeug(suv).getFlaeche());
+        assertEquals(6,FahrzeugtypHelper.getFahrzeug(pickup).getFlaeche());
     }
 }
