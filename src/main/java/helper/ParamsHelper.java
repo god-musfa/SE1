b64fe9986f677e36c2fbf9769f67d96d75c4aff3
cedricbetke <cedricbetke@gmail.com>
2022-07-04 19:13:44 +0200

    Parameterhelper für den Params Array

---
+++ src/main/java/helper/ParamsHelper.java
@@ -0,0 +1,40 @@
+package helper;
+
+public class ParamsHelper {
+    public static String getNr(String[] params){
+        return params[0];
+    }
+    public static String getTimer(String[] params){
+        return params[1];
+    }
+    public static String getDuration(String[] params){
+        return params[2];
+    }
+    public static String getPrice(String[] params){
+        return params[3];
+    }
+    public static String getHashForTicket(String[] params){
+        return params[4];
+    }
+    public static String getColor(String[] params){
+        return params[5];
+    }
+    public static String getSpace(String[] params){
+        return params[6];
+    }
+    public static String getClient_category(String[] params){
+        return params[7];
+    }
+    public static String getVehicle_type(String[] params){
+        return params[8];
+    }
+    public static String getLicense(String[] params){
+        return params[9];
+    }
+    public static String getBegin(String[] params){
+        return params[10];
+    }
+    public static String getparkhaus_name(String[] params){
+        return params[11];
+    }
+}
---
+++ src/test/java/helper/ParamsHelperTest.java
@@ -0,0 +1,75 @@
+package helper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParamsHelperTest {
+    String[] params;
+    @BeforeEach
+    void setUp() {
+    params = new String[]{"2","1656953867960","_","_","79978abed24073fea1acdac913a069ab","#402513","5","Frau","SUV","SU-V 15","1656953921978","Etage1"};
+    }
+
+    @Test
+    void getNr() {
+        assertEquals("2",ParamsHelper.getNr(params));
+    }
+
+    @Test
+    void getTimer() {
+        assertEquals("1656953867960",ParamsHelper.getTimer(params));
+    }
+
+    @Test
+    void getDuration() {
+        assertEquals("_",ParamsHelper.getDuration(params));
+    }
+
+    @Test
+    void getPrice() {
+        assertEquals("_",ParamsHelper.getPrice(params));
+    }
+
+    @Test
+    void getHashForTicket() {
+        assertEquals("79978abed24073fea1acdac913a069ab",ParamsHelper.getHashForTicket(params));
+    }
+
+    @Test
+    void getColor() {
+        assertEquals("#402513",ParamsHelper.getColor(params));
+
+    }
+    @Test
+    void getSpace() {
+        assertEquals("5",ParamsHelper.getSpace(params));
+    }
+    @Test
+    void getClient_category() {
+        assertEquals("Frau",ParamsHelper.getClient_category(params));
+    }
+
+    @Test
+    void getVehicle_type() {
+        assertEquals("SUV",ParamsHelper.getVehicle_type(params));
+    }
+
+    @Test
+    void getLicense() {
+        assertEquals("SU-V 15",ParamsHelper.getLicense(params));
+    }
+
+    @Test
+    void getBegin() {
+        assertEquals("1656953921978",ParamsHelper.getBegin(params));
+    }
+
+    @Test
+    void getparkhaus_name() {
+        assertEquals("Etage1",ParamsHelper.getparkhaus_name(params));
+
+    }
+
+}
