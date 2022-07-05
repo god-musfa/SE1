0ea5ce81497f52c1c1933c6d3e5236151d994450
Mustafa Tarik <mustafa.tarik@smail.inf.h-brs.de>
2022-07-05 19:28:28 +0200

    Fixed code smells

--- src/main/java/template/GroupedIncomeStatistic.java
+++ src/main/java/template/GroupedIncomeStatistic.java
@@ -1,7 +1,6 @@
 package template;
 
 import base.CarIF;
-import jakarta.json.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +19,6 @@ public class GroupedIncomeStatistic extends ChartStatistic {
             }
              else {
                 int finalI = i;
-                System.out.println(finalI);
                 count.add((int) car.stream()
                         .filter((CarIF c) -> (c.getTicket().getPrice() >= Integer.parseInt(strings) && c.getTicket().getPrice() < Integer.parseInt(label.get(finalI))))
                         .count());
@@ -31,6 +29,6 @@ public class GroupedIncomeStatistic extends ChartStatistic {
     }
     @Override
     public List<String> calcValues(List<CarIF> car) {
-        return List.of(new String[]{"5", "10", "15"});
+        return List.of("5", "10", "15");
     }
 }
