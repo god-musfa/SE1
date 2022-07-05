5085c63b234fd5a2b2ef0386afdded548c01c10d
cedricbetke <cedricbetke@gmail.com>
2022-07-05 19:56:12 +0200

    Todo entfernt

--- src/test/java/base/EinnahmenStatistikTagesEinnahmenTest.java
+++ src/test/java/base/EinnahmenStatistikTagesEinnahmenTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EinnahmenStatistikTagesEinnahmenTest { // TODO Statistiken zusammenführen
+class EinnahmenStatistikTagesEinnahmenTest {
 ArrayList<TicketIF> tickets;
 StatistikTagesEinnahmen tages;
     @BeforeEach
